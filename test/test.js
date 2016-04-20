11d128c82553e9ebb055cfd462ca42ca50715eba
alexcrack <a13xcrack@gmail.com>
2016-04-20 17:39:40 +0300

    test(all): add tests and travis ci

--- test/test.js
+++ test/test.js
@@ -1,5 +1,23 @@
-var romanize = require("./../index.js");
+const expect = require('chai').expect;
+const romanize = require("./../index.js");
 
 
-var orig = "хоккей";
-console.log(orig, romanize(orig));
+describe('Test romanization', () => {
+   
+    describe('Cyrillic text', () => {
+
+        it('should romanize russion', () => {
+            expect(romanize("Хоккей")).to.equal("Hokkey");
+        });
+
+    });
+
+    describe('Kazakh text', () => {
+
+        it('should romanize kazakh', () => {
+            expect(romanize("Сіз қазақ тілін әлбетте таңдап қойдыңыз")).to.equal("Siz qazaq tilin a'lbette tanhdap qoydynhyz");
+        });
+
+    });
+    
+});
